04496e1ae62bc2faa29991cb5340ad23bd898b5f
Jose <backend2.muchaweb@gmail.com>
2015-10-20 11:46:23 -0500

    fix bad group queries

--- jet/dashboard/utils.py
+++ jet/dashboard/utils.py
@@ -1,15 +1,19 @@
 from importlib import import_module
 from django.contrib.auth.models import Group
 from jet.dashboard import settings
-
-clients_group = Group.objects.get(name='Clientes')
-managers_group = Group.objects.get(name='Managers')
+try:
+    clients_group = Group.objects.get(name='Clientes')
+except:
+    clients_group = None
+try:
+    managers_group = Group.objects.get(name='Managers')
+except:
+    managers_group = None
 
 def get_current_dashboard(location, user):
 
     if location == 'index':
         if user.is_superuser:
-            print(user)
             path = settings.JET_INDEX_DASHBOARD
         elif clients_group in user.groups.all():
             path = settings.JET_INDEX_CLIENT_DASHBOARD
@@ -25,10 +29,8 @@ def get_current_dashboard(location, user):
             path = settings.JET_APP_INDEX_MANAGER_DASHBOARD
     else:
         raise ValueError('Unknown dashboard location: %s' % location)
-    print(path)
     module, cls = path.rsplit('.', 1)
     module = import_module(module)
     index_dashboard_cls = getattr(module, cls)
-    print(index_dashboard_cls)
 
     return index_dashboard_cls
